f5d49d1a4731f8c07a47186e218a6c4caa9dc420
Bryan <bryanlima200@outlook.com>
2021-06-24 22:18:03 -0300

    Added data output exercises

--- section-03/section3.exercises/section3.exercises/Program.cs
+++ section-03/section3.exercises/section3.exercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace section3.exercises
 {
@@ -6,7 +7,29 @@ namespace section3.exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string product1 = "Computador"; ;
+            string product2 = "Mesa de escritório";
+
+            byte age = 30;
+            int code = 5290;
+            char gender = 'M';
+
+            double price1 = 2100.0;
+            double price2 = 650.50;
+            double measure = 53.234567;
+
+
+            Console.WriteLine("\nPRODUTOS");
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine($"{product1}, cujo preço é $ {price1:F2}");
+            Console.WriteLine($"{product2}, cujo preço é $ {price2:F2}\n\n");
+
+            Console.WriteLine($"Registro: {age} anos de idade, código {code} e gênero: {gender}\n\n");
+
+            Console.WriteLine($"Medida com oito casas decimais: {measure:F8}");
+            Console.WriteLine($"Arredondado (três casas decimais): {measure:F3}");
+            Console.WriteLine("Separador decimal invariante culture: " + measure.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("---------------------------------------------------");
         }
     }
 }
