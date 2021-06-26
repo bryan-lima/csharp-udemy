351b09cd47c1614262d37bfa600f3ea6d951db4e
Bryan <bryanlima200@outlook.com>
2021-06-25 21:25:15 -0300

    Added data input exercises

---
+++ section-03/section3.exercises/section3.exercises/DataInput.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace section3.exercises
+{
+    class DataInput
+    {
+        public static void CSharpDataInput()
+        {
+            Console.WriteLine("\nFORMULÁRIO DE CADASTRO");
+            Console.WriteLine("------------------------------------------------------------\n");
+
+            Console.WriteLine("Entre com seu nome completo:");
+            Console.Write(">> ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("\nQuantos quartos tem na sua casa?");
+            Console.Write(">> ");
+            int numberOfRooms = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nEntre com o preço de um produto:");
+            Console.Write(">> ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("\nEntre seu último nome, idade e altura (mesma linha):");
+            Console.Write(">> ");
+            string[] longAnswer = Console.ReadLine().Split(' ');
+
+            string lastName = longAnswer[0];
+            int age = int.Parse(longAnswer[1]);
+            double height = double.Parse(longAnswer[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("------------------------------------------------------------\n");
+
+            Console.WriteLine($"Nome: {name}");
+            Console.WriteLine($"Quantidade quartos: {numberOfRooms}");
+            Console.WriteLine("Preço do produto: " + price.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine($"Último nome: {lastName}");
+            Console.WriteLine($"Idade: {age}");
+            Console.WriteLine("Altura: " + height.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
--- section-03/section3.exercises/section3.exercises/Program.cs
+++ section-03/section3.exercises/section3.exercises/Program.cs
@@ -8,7 +8,9 @@ namespace section3.exercises
     {
         static void Main(string[] args)
         {
-            DataOutput.CSharpDataOutput(); // Aula 18. Saída de dados em C#
+            //DataOutput.CSharpDataOutput(); // Aula 18. Saída de dados em C#
+
+            DataInput.CSharpDataInput(); // Aula 23. Entrada de dados em C#
         }
     }
 }
