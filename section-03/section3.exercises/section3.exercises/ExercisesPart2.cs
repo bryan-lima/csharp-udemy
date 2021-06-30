0296ba937c415a27317d72ff38093861fee0ed03
Bryan <bryanlima200@outlook.com>
2021-06-29 21:01:27 -0300

    Added proposed exercises part 2 - Exercise 4

--- section-03/section3.exercises/section3.exercises/ExercisesPart2.cs
+++ section-03/section3.exercises/section3.exercises/ExercisesPart2.cs
@@ -86,5 +86,28 @@ namespace section3.exercises
                 Console.WriteLine("\nOs números informados não são múltiplos entre si");
             }
         }
+
+        public static void Ex4()
+        {
+            //Leia a hora inicial e a hora final de um jogo
+            //A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas
+
+            Console.WriteLine("\nCONTAR HORAS DE JOGO");
+            Console.WriteLine("-----------------------");
+            Console.Write("\nHora inicial (24hr): ");
+            int startTime = int.Parse(Console.ReadLine());
+            Console.Write("\nHora final (24hr): ");
+            int endTime = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n-----------------------");
+
+            int amountFfGameHours = (24 - startTime) + endTime;
+
+            if (amountFfGameHours > 24)
+            {
+                amountFfGameHours -= 24;
+            }
+
+            Console.WriteLine($"\nO jogo durou {amountFfGameHours} hora(s)");
+        }
     }
 }
