697516e5bb3a79499bf6730c85f57dd8ff7fd92b
Bryan <bryanlima200@outlook.com>
2021-07-06 21:28:54 -0300

    Added proposed exercises part 4 - Exercise 7

--- section-03/section3.exercises/section3.exercises/ExercisesPart4.cs
+++ section-03/section3.exercises/section3.exercises/ExercisesPart4.cs
@@ -188,5 +188,20 @@ namespace section3.exercises
                 }
             }
         }
+
+        public static void Ex7()
+        {
+            //Fazer um programa para ler um número inteiro positivo N
+            //O programa deve então mostrar na tela N linhas, começando de 1 até N
+            //Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme exemplo
+
+            Console.Write("\nInforme um número inteiro positivo: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"{i} {i * i} {i * i * i}");
+            }
+        }
     }
 }
