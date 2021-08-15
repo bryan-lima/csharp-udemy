a613b8d51a2b729618202720adc882d1714901ff
Bryan <bryanlima200@outlook.com>
2021-08-15 19:12:32 -0300

    WorkerLevel enumerator implemented

--- section-09/section9.exercises/section9.exercises/Entities/Enums/WorkerLevel.cs
+++ section-09/section9.exercises/section9.exercises/Entities/Enums/WorkerLevel.cs
@@ -4,7 +4,10 @@ using System.Text;
 
 namespace section9.exercises.Entities.Enums
 {
-    class WorkerLevel
+    enum WorkerLevel : int
     {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
     }
 }
