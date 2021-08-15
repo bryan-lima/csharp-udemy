22bc6e83e9293b86315db2abe82f668ee16edede
Bryan <bryanlima200@outlook.com>
2021-08-15 19:11:58 -0300

    Class HourContract implemented

--- section-09/section9.exercises/section9.exercises/Entities/HourContract.cs
+++ section-09/section9.exercises/section9.exercises/Entities/HourContract.cs
@@ -6,5 +6,27 @@ namespace section9.exercises.Entities
 {
     class HourContract
     {
+        public DateTime Date { get; set; }
+
+        public double ValuePerHour { get; set; }
+
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return ValuePerHour * Hours;
+        }
     }
 }
