df36f2681c0a321ae70c86ad2ea619a17336b9cd
Bryan <bryanlima200@outlook.com>
2021-08-15 22:05:27 -0300

    Class Post implemented

--- section-09/section9.exercises/section9.exercises/Entities/Post.cs
+++ section-09/section9.exercises/section9.exercises/Entities/Post.cs
@@ -6,5 +6,37 @@ namespace section9.exercises.Entities
 {
     class Post
     {
+        public DateTime Moment { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int Likes { get; set; }
+
+        public List<Comment> Comments { get; private set; } = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
     }
 }
