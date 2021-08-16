d02d1be7047dd76f715160de67afe24aad843e1c
Bryan <bryanlima200@outlook.com>
2021-08-15 22:27:41 -0300

    Added secondary exercise using StringBuilder

--- section-09/section9.exercises/section9.exercises/Program.cs
+++ section-09/section9.exercises/section9.exercises/Program.cs
@@ -6,7 +6,9 @@ namespace section9.exercises
     {
         static void Main(string[] args)
         {
-            MainExercise.Exercise();    // Aulas 119, 120 e 121. Exercício resolvido 01 - Partes 1, 2 e 3
+            //MainExercise.Exercise();    // Aulas 119, 120 e 121. Exercício resolvido 01 - Partes 1, 2 e 3
+            
+            SecondaryExercise.Exercise();   // Aula 122. Exercício resolvido 02 (demo StringBuilder)
         }
     }
 }
--- section-09/section9.exercises/section9.exercises/SecondaryExercise.cs
+++ section-09/section9.exercises/section9.exercises/SecondaryExercise.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using section9.exercises.Entities;
 
 namespace section9.exercises
 {
     class SecondaryExercise
     {
+        public static void Exercise()
+        {
+            #region Firs Post
+            Comment firstPostFirstComment = new Comment("Have a nice trip");
+            Comment firstPostSecondComment = new Comment("Wow that's awesome!");
+
+            Post firstPost = new Post(DateTime.Parse("21/06/2018 13:05:44"), "Traveling to New Zealand", "I'm going to visit this wonderful country!", 12);
+
+            firstPost.AddComment(firstPostFirstComment);
+            firstPost.AddComment(firstPostSecondComment);
+            #endregion
+
+            #region Second Post
+            Comment secondPostFirstComment = new Comment("Have a nice trip");
+            Comment secondPostSecondComment = new Comment("Wow that's awesome!");
+
+            Post secondPost = new Post(DateTime.Parse("21/06/2018 13:05:44"), "Traveling to New Zealand", "I'm going to visit this wonderful country!", 12);
+
+            secondPost.AddComment(secondPostFirstComment);
+            secondPost.AddComment(secondPostSecondComment);
+            #endregion
+
+            Console.WriteLine(firstPost);
+            Console.WriteLine(secondPost);
+        }
     }
 }
