ea719660582c362eb06357da08c4c55cd1ff102a
Bryan <bryanlima200@outlook.com>
2021-08-16 23:48:52 -0300

    Solved proposed exercise

--- section-09/section9.exercises/section9.exercises/ProposedExercise.cs
+++ section-09/section9.exercises/section9.exercises/ProposedExercise.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
+using section9.exercises.Entities;
+using section9.exercises.Entities.Enums;
 
 namespace section9.exercises
 {
@@ -47,11 +50,59 @@ namespace section9.exercises
                                                                                        |                                                          |
                                                                                        +----------------------------------------------------------+
             
-             */
+            */
 
+            #region Client Data
+            Console.WriteLine("\nEnter client data:");
+            Console.WriteLine("------------------");
+            
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
 
+            Console.Write("Email: ");
+            string clientEmail = Console.ReadLine();
 
+            Console.Write("Birth Date (DD/MM/YYYY): ");
+            DateTime clientBirthDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
 
+            Client client = new Client(clientName,clientEmail, clientBirthDate);
+            #endregion
+
+            #region Order Data
+            Console.WriteLine("\n\nEnter order data:");
+            Console.WriteLine("-----------------");
+
+            Console.Write("Status: ");
+            OrderStatus orderStatus = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, orderStatus, client);
+
+            Console.Write("\nHow many items to this order? ");
+            int quantityItems = int.Parse(Console.ReadLine());
+
+            for(int item = 1; item <= quantityItems; item++)
+            {
+                Console.WriteLine($"\nEnter {item}# item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+
+                Console.Write("Product price: $ ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Quantity: ");
+                int productQuantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(productName, productPrice);
+                OrderItem orderItem = new OrderItem(productQuantity, productPrice, product);
+                order.AddItem(orderItem);
+            }
+            #endregion
+
+            #region Order Summary
+            Console.WriteLine("\n\n                    ORDER SUMMARY");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine(order);
+            #endregion
         }
     }
 }
