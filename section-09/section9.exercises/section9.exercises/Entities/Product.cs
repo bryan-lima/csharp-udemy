42385d5d4c373e3189f88a1dc85ed8b29246e8c4
Bryan <bryanlima200@outlook.com>
2021-08-16 22:16:06 -0300

    Class Product implemented

--- section-09/section9.exercises/section9.exercises/Entities/Product.cs
+++ section-09/section9.exercises/section9.exercises/Entities/Product.cs
@@ -6,5 +6,19 @@ namespace section9.exercises.Entities
 {
     class Product
     {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
     }
 }
