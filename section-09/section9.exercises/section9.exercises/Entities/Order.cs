acd70b898a831a70e06bf7db143c8b1a6e3af1b8
Bryan <bryanlima200@outlook.com>
2021-08-16 22:38:39 -0300

    Class Order implemented

--- section-09/section9.exercises/section9.exercises/Entities/Order.cs
+++ section-09/section9.exercises/section9.exercises/Entities/Order.cs
@@ -1,10 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using section9.exercises.Entities.Enums;
 
 namespace section9.exercises.Entities
 {
     class Order
     {
+        public DateTime Moment { get; set; }
+
+        public OrderStatus Status { get; set; }
+
+        public Client Client { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0.0;
+
+            foreach (OrderItem orderItem in Items)
+            {
+                total += orderItem.SubTotal();
+            }
+
+            return total;
+        }
     }
 }
