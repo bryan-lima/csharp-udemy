be88de301342a55334456e9b86e5b5aaf3d7a772
Bryan <bryanlima200@outlook.com>
2021-08-16 22:37:55 -0300

    Class Client implemented

--- section-09/section9.exercises/section9.exercises/Entities/Client.cs
+++ section-09/section9.exercises/section9.exercises/Entities/Client.cs
@@ -6,5 +6,22 @@ namespace section9.exercises.Entities
 {
     class Client
     {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public Client()
+        {
+
+        }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
     }
 }
