914fe63795c8eef3ab589170576579b9b2fd47d7
Bryan <bryanlima200@outlook.com>
2021-08-18 23:35:59 -0300

    Started construction of the proposed exercise - inheritance and polymorphism

--- section-10/section10.exercises/Exercise.InheritanceAndPolymorphism/Program.cs
+++ section-10/section10.exercises/Exercise.InheritanceAndPolymorphism/Program.cs
@@ -6,7 +6,45 @@ namespace Exercise.InheritanceAndPolymorphism
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+
+            Fazer um programa para ler os dados de N produtos(N fornecido pelo usuário).
+            Ao final, mostrar a etiqueta de preço de cada produto na mesma ordem em que foram digitados.
+
+            Todo produto possui nome e preço. 
+            Produtos importados possuem uma taxa de alfândega, e produtos usados possuem data de fabricação.
+            Estes dados específicos devem ser acrescentados na etiqueta de preço conforme exemplo(próxima página).
+            Para produtos importados, a taxa e alfândega deve ser acrescentada ao preço final do produto.
+
+            Favor implementar o programa conforme projeto abaixo.
+
+
+                                         +------------------------+
+                                         |        Product         |
+                                         +------------------------+
+                                         | - name: String         |
+                                         | - price: Double        |
+                                         +------------------------+
+                                         | + priceTag(): String   |
+                                         +------------------------+
+                                                     /|\
+                                                      |
+                                   ___________________|___________________
+                                   |                                     |
+                                   |                                     |
+                     +---------------------------+        +---------------------------+
+                     |      ImportedProduct      |        |        UsedProduct        |
+                     +---------------------------+        +---------------------------+
+                     | - customFee: Double       |        | - manufactureDate: Date   |
+                     +---------------------------+        +---------------------------+
+                     | + priceTag(): String      |        | + priceTag(): String      |
+                     | + totalPrice(): Double    |        +---------------------------+
+                     +---------------------------+
+
+
+            */
+
+
         }
     }
 }
