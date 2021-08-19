efa7c72afa371eb414e8b2cc9ea8d11816ba6f9a
Bryan <bryanlima200@outlook.com>
2021-08-18 23:41:11 -0300

    ImportedProduct class implemented

--- section-10/section10.exercises/Exercise.InheritanceAndPolymorphism/Entities/ImportedProduct.cs
+++ section-10/section10.exercises/Exercise.InheritanceAndPolymorphism/Entities/ImportedProduct.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace Exercise.InheritanceAndPolymorphism.Entities
 {
-    class ImportedProduct
+    class ImportedProduct : Product
     {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+
+        }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return base.PriceTag() + " (Customs fee: $ " + CustomsFee.ToString("F2", CultureInfo.InvariantCulture) + ")";
+        }
     }
 }
