09ba1ad9f1a13aca917b3c923237a331ba9b353e
Bryan <bryanlima200@outlook.com>
2021-08-18 22:37:16 -0300

    Developed exercise before correction

--- section-10/section10.exercises/section10.exercises/SolvedExercise.cs
+++ section-10/section10.exercises/section10.exercises/SolvedExercise.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using section10.exercises.Entities;
 
 namespace section10.exercises
 {
@@ -19,8 +21,8 @@ namespace section10.exercises
             Fazer um programa para ler os dados de N funcionários(N fornecido pelo usuário) e armazená-los em uma lista.
             Depois de ler todos os dados, mostrar nome e pagamento de cada funcionário na mesma ordem em que foram digitados.
             
-            Construa o programa conforme projeto ao lado.
-            Veja exemplo na próxima página.
+            Construa o programa conforme projeto abaixo.
+
 
                       +---------------------------+
                       |          Employee         |
@@ -43,7 +45,80 @@ namespace section10.exercises
 
             */
 
+            Console.Write("\nEnter the number of employees: ");
+            int numberOfEmployees = int.Parse(Console.ReadLine());
 
+            List<Employee> employeeList = new List<Employee>();
+
+            for (int i = 1; i <= numberOfEmployees; i++)
+            {
+                Console.WriteLine($"\n\nEmployee #{i} data:");
+                Console.WriteLine("-----------------");
+
+                bool hasOutsourced = false;
+
+                while (true)
+                {
+                    Console.Write("\nOutsourced (y/n)? ");
+                    string answerIfIsOutsourced = Console.ReadLine();
+
+                    if (answerIfIsOutsourced.Trim().ToUpper() == "Y")
+                    {
+                        hasOutsourced = true;
+                        break;
+                    }
+                    else if (answerIfIsOutsourced.Trim().ToUpper() == "N")
+                    {
+                        hasOutsourced = false;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid answer! Just type Y or N.");
+                    }
+                }
+
+                Console.Write("Name: ");
+                string employeeName = Console.ReadLine();
+
+                Console.Write("Hours: ");
+                int employeeHours = int.Parse(Console.ReadLine());
+
+                Console.Write("Value per hour: $ ");
+                double employeeValuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Employee employee;
+
+                if (hasOutsourced)
+                {
+                    Console.Write("Additional charge: $ ");
+                    double outsourcedEmployeeAdditionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    employee = new OutsourcedEmployee(employeeName, employeeHours, employeeValuePerHour, outsourcedEmployeeAdditionalCharge);
+                }
+                else
+                {
+                    employee = new Employee(employeeName, employeeHours, employeeValuePerHour);
+                }
+
+                employeeList.Add(employee);
+            }
+
+            Console.WriteLine("\n\n\n                    PAYMENTS:");
+            Console.WriteLine("-------------------------------------------------");
+
+            foreach (Employee employee in employeeList)
+            {
+                if (employee is Employee)
+                {
+                    Console.WriteLine(employee);
+                }
+                else
+                {
+                    OutsourcedEmployee outsourcedEmployee = employee as OutsourcedEmployee;
+                    Console.WriteLine(outsourcedEmployee);
+                }
+            }
         }
     }
 }
