92be44679004f567c5368ab54085229eb232b117
Bryan <bryanlima200@outlook.com>
2021-08-19 00:24:24 -0300

    Solved proposed exercise

--- section-10/section10.exercises/Exercise.InheritanceAndPolymorphism/Program.cs
+++ section-10/section10.exercises/Exercise.InheritanceAndPolymorphism/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Exercise.InheritanceAndPolymorphism.Entities;
 
 namespace Exercise.InheritanceAndPolymorphism
 {
@@ -35,7 +38,7 @@ namespace Exercise.InheritanceAndPolymorphism
                      +---------------------------+        +---------------------------+
                      |      ImportedProduct      |        |        UsedProduct        |
                      +---------------------------+        +---------------------------+
-                     | - customFee: Double       |        | - manufactureDate: Date   |
+                     | - customsFee: Double      |        | - manufactureDate: Date   |
                      +---------------------------+        +---------------------------+
                      | + priceTag(): String      |        | + priceTag(): String      |
                      | + totalPrice(): Double    |        +---------------------------+
@@ -44,7 +47,66 @@ namespace Exercise.InheritanceAndPolymorphism
 
             */
 
+            Console.Write("\n\nEnter the number of products: ");
+            int quantityProducts = int.Parse(Console.ReadLine());
 
+            List<Product> productList = new List<Product>();
+
+            for (int i = 1; i <= quantityProducts; i++)
+            {
+                Console.WriteLine($"\n\nProduct #{i} data:");
+                Console.WriteLine("----------------");
+
+                string productType;
+
+                while (true)
+                {
+                    Console.Write("\nCommom, used or imported (c/u/i)? ");
+                    productType = Console.ReadLine().Trim().ToUpper();
+
+                    if (productType == "C" || productType == "U" || productType == "I")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid answer! Just type C for Common, or U for Used, or I for Imported.");
+                    }
+                }
+
+                Console.Write("Name: ");
+                string productName = Console.ReadLine();
+
+                Console.Write("Price: $ ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (productType == "U")
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime productManufactureDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+
+                    productList.Add(new UsedProduct(productName, productPrice, productManufactureDate));
+                }
+                else if (productType == "I")
+                {
+                    Console.Write("Customs fee: $ ");
+                    double productCustomsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    productList.Add(new ImportedProduct(productName, productPrice, productCustomsFee));
+                }
+                else
+                {
+                    productList.Add(new Product(productName, productPrice));
+                }
+            }
+
+            Console.WriteLine("\n\n\n                    PRICE TAGS:");
+            Console.WriteLine("---------------------------------------------------");
+
+            foreach (Product product in productList)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
         }
     }
 }
