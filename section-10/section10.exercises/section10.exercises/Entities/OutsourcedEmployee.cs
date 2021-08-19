076224cf9b9d4e9e90f8ecedc0c9f98ac7f829c8
Bryan <bryanlima200@outlook.com>
2021-08-18 21:50:04 -0300

    OutsourcedEmployee class implemented

--- section-10/section10.exercises/section10.exercises/Entities/OutsourcedEmployee.cs
+++ section-10/section10.exercises/section10.exercises/Entities/OutsourcedEmployee.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace section10.exercises.Entities
 {
-    class OutsourcedEmployee
+    class OutsourcedEmployee : Employee
     {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return Hours * ValuePerHour + AdditionalCharge * 1.1;
+        }
     }
 }
