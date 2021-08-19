db8674fb72f86fa682d2c59451963fe428970a2f
Bryan <bryanlima200@outlook.com>
2021-08-18 21:48:06 -0300

    Employee class implemented

--- section-10/section10.exercises/section10.exercises/Entities/Employee.cs
+++ section-10/section10.exercises/section10.exercises/Entities/Employee.cs
@@ -6,5 +6,27 @@ namespace section10.exercises.Entities
 {
     class Employee
     {
+        public string Name { get; set; }
+
+        public int Hours { get; set; }
+
+        public double ValuePerHour { get; set; }
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
     }
 }
