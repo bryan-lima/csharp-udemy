e786ba53cc3aa3642fb4622605200f09a5dcc569
Bryan <bryanlima200@outlook.com>
2021-08-19 23:36:12 -0300

    Circle class implemented

--- section-10/section10.exercises/section10.exercises/Entities/Circle.cs
+++ section-10/section10.exercises/section10.exercises/Entities/Circle.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using section10.exercises.Entities.Enums;
 
 namespace section10.exercises.Entities
 {
-    class Circle
+    class Circle : Shape
     {
+        public double Radios { get; set; }
+
+        public Circle(double radios, Color color) :  base(color)
+        {
+            Radios = radios;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radios * Radios;
+        }
     }
 }
