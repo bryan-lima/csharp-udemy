bcc665cf26aa19782883e83af0a1000e352fc96c
Bryan <bryanlima200@outlook.com>
2021-08-19 23:33:33 -0300

    Rectangle class implemented

--- section-10/section10.exercises/section10.exercises/Entities/Rectangle.cs
+++ section-10/section10.exercises/section10.exercises/Entities/Rectangle.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using section10.exercises.Entities.Enums;
 
 namespace section10.exercises.Entities
 {
-    class Rectangle
+    class Rectangle : Shape
     {
+        public double With { get; set; }
+
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height, Color color) : base(color)
+        {
+            With = width;
+            Height = height;
+        }
+
+        public override double Area()
+        {
+            return With * Height;
+        }
     }
 }
