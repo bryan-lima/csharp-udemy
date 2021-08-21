2ad3c5b15e7bc9649aad29468b92791e1511048e
Bryan <bryanlima200@outlook.com>
2021-08-20 23:17:56 -0300

    Company class implemented

--- section-10/section10.exercises/Exercise.AbstractMethods/Entities/Company.cs
+++ section-10/section10.exercises/Exercise.AbstractMethods/Entities/Company.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Exercise.AbstractMethods.Entities
 {
-    class Company
+    class Company : TaxPayer
     {
+        public int NumberOfEmployees { get; set; }
+
+        public Company(string name, double annualIncome, int numberOfEmployees) :  base(name, annualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double TaxToPay()
+        {
+            double percentageOfTaxPayable = 0.16;
+
+            if (NumberOfEmployees > 10)
+            {
+                percentageOfTaxPayable = 0.14;
+            }
+
+            return AnnualIncome * percentageOfTaxPayable;
+        }
     }
 }
