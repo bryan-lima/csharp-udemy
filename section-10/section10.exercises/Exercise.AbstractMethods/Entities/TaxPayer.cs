2cd7e8b4099316be28963379b96b6085a3121117
Bryan <bryanlima200@outlook.com>
2021-08-20 23:17:08 -0300

    TaxPayer class implemented

--- section-10/section10.exercises/Exercise.AbstractMethods/Entities/TaxPayer.cs
+++ section-10/section10.exercises/Exercise.AbstractMethods/Entities/TaxPayer.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace Exercise.AbstractMethods.Entities
 {
-    class TaxPayer
+    abstract class TaxPayer
     {
+        public string Name { get; set; }
+
+        public double AnnualIncome { get; set; }
+
+        public TaxPayer(string name, double annualIncome)
+        {
+            Name = name;
+            AnnualIncome = annualIncome;
+        }
+
+        public abstract double TaxToPay();
+
+        public override string ToString()
+        {
+            return Name + ": $ " + TaxToPay().ToString("F2", CultureInfo.InvariantCulture);
+        }
     }
 }
