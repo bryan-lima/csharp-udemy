9c5036774cbc9b8f17cf06a8cadeb9daba5a5be9
Bryan <bryanlima200@outlook.com>
2021-08-20 23:17:33 -0300

    Individual class implemented

--- section-10/section10.exercises/Exercise.AbstractMethods/Entities/Individual.cs
+++ section-10/section10.exercises/Exercise.AbstractMethods/Entities/Individual.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace Exercise.AbstractMethods.Entities
 {
-    class Individual
+    class Individual : TaxPayer
     {
+        public double MedicalExpenses { get; set; }
+
+        public Individual(string name, double annualIncome, double medicalExpenses) : base(name, annualIncome)
+        {
+            MedicalExpenses = medicalExpenses;
+        }
+
+        public override double TaxToPay()
+        {
+            double percentageOfTaxPayable = 0.15;
+
+            if (AnnualIncome >= 20000.0)
+            {
+                percentageOfTaxPayable = 0.25;
+            }
+
+            if (MedicalExpenses > 0.0)
+            {
+                return (AnnualIncome * percentageOfTaxPayable) - (MedicalExpenses * 0.5);
+            }
+            else
+            {
+                return (AnnualIncome * percentageOfTaxPayable);
+            }
+        }
     }
 }
