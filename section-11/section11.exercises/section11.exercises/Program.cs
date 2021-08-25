2b9d486760a703435e53c735007193fe1b3aaff8
Bryan <bryanlima200@outlook.com>
2021-08-24 22:04:38 -0300

    Added section11.exercises project

---
+++ section-11/section11.exercises/section11.exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace section11.exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
