68041f39b6698f2980e58ceba28cb32a4798e590
Bryan <bryanlima200@outlook.com>
2021-08-25 00:13:31 -0300

    Started resolution of proposed exercise - Handling Exceptions

--- section-11/section11.exercises/FixationExercise/Program.cs
+++ section-11/section11.exercises/FixationExercise/Program.cs
@@ -6,7 +6,28 @@ namespace FixationExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            
+            Fazer um programa para ler os dados de uma conta bancária e depois realizar um saque nesta conta bancária, mostrando o novo saldo.
+            Um saque não pode ocorrer ou se não houver saldo na conta, ou se o valor do saque for superior ao limite de saque da conta.
+            Implemente a conta bancária conforme projeto abaixo:
+
+
+                                        +-------------------------------------+
+                                        |               Account               |
+                                        +-------------------------------------+
+                                        | - number: Integer                   |
+                                        | - holder: String                    |
+                                        | - balance: Double                   |
+                                        | - withdrawLimit: Double             |
+                                        +-------------------------------------+
+                                        | + deposit(amount: Double): void     |
+                                        | + withdraw(amount: Double): void    |
+                                        +-------------------------------------+
+
+            */
+
+
         }
     }
 }
