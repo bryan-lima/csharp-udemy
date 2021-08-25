f2a76e956a6e769156e7ceaadde84ef0ad90925c
Bryan <bryanlima200@outlook.com>
2021-08-24 23:28:56 -0300

    First solution of the exercise (very bad solution) Solution 1 (very bad): validation logic in main program - Validation logic not delegated to reservation

--- section-11/section11.exercises/section11.exercises/Program.cs
+++ section-11/section11.exercises/section11.exercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using section11.exercises.Entities;
 
 namespace section11.exercises
 {
@@ -29,9 +30,50 @@ namespace section11.exercises
                             +--------------------------------------------------------+
 
 
-             */
+            */
 
+            Console.Write("\nRoom number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
 
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("\nError in reservation: Check-out date must be after check-in date!");
+            }
+            else
+            {
+
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine($"\nReservation: {reservation}");
+
+                Console.WriteLine("\nEnter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+
+                if (checkIn < now || checkOut < now)
+                {
+                    Console.WriteLine("\nError in reservation: Reservation dates for update must be future dates!");
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("\nError in reservation: Check-out date must be after check-in date!");
+                }
+                else
+                {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine($"\nReservation: {reservation}");
+                }
+            }
         }
     }
 }
