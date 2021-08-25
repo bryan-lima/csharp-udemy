64070e37ae0bcd2c3ec078af4a177aee5d90adc1
Bryan <bryanlima200@outlook.com>
2021-08-25 00:19:10 -0300

    DomainException class implemented

--- section-11/section11.exercises/FixationExercise/Entities/Exceptions/DomainException.cs
+++ section-11/section11.exercises/FixationExercise/Entities/Exceptions/DomainException.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace FixationExercise.Entities.Exceptions
 {
-    class DomainException
+    class DomainException : ApplicationException
     {
+        public DomainException(string message) : base(message)
+        {
+
+        }
     }
 }
